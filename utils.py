02bf8f5a3160ad69167eeb11dde8fef868786412
Pablo Hueso <pablohuesomerino@gmail.com>
2025-05-12 10:33:21 +0200

    batched attack function and streamline notebook

--- utils.py
+++ utils.py
@@ -17,14 +17,13 @@ def jacobian_batch(model, x_batch):
     J_diag = J_diag.reshape(B, nb_classes, -1)        # (B,nb_classes,CxHxW)
     return J_diag
 
-def flipping_vector(model, x_batch, attacked_class): 
+def flipping_vector(model, x_batch, attacked_class=2): 
     model_output = model(x_batch) # [B, c]
     B, c = model_output.shape
 
     assert 2 <= attacked_class <= c, "attacked_class must be in 2,...,c"
 
     largest_logits = torch.topk(model_output, k=attacked_class, dim=1).indices
-    flipping_vector = torch.zeros(model_output.shape) # zero tensor of shape [B, c]
 
     # i: index of max logit per sample (argmax) -> [B]
     i = largest_logits[:, 0]
@@ -32,6 +31,7 @@ def flipping_vector(model, x_batch, attacked_class):
     # j: index of attacked_class-th largest logit -> [B]
     j = largest_logits[:, -1] 
     
+
     flipping = torch.zeros_like(model_output)  # [B, c]
 
     batch_indices = torch.arange(B) #vector [0, 1, ... , B-1]
@@ -39,6 +39,14 @@ def flipping_vector(model, x_batch, attacked_class):
     flipping[batch_indices, i] = -1
     flipping[batch_indices, j] = 1
 
-    return flipping
+    return flipping 
 
-#def ocf_attack(model, batch)
+def ocf_attack(model, batch, attacked_class=2):
+    B, C, H, W = batch.shape # Batch size, number of channels, height and width respectively
+    jacobian_b = jacobian_batch(model, batch) # jacobian_b has shape [B, nb_classes, CXHXW]
+    J_pinv = torch.linalg.pinv(jacobian_b) # [B, CxHxW, nb_classes]
+    flips = flipping_vector(model, batch, 2) # [B, c] (B flipping vectors)
+    attack = torch.bmm(J_pinv, flips.unsqueeze(2)) # Add unsqueeze to flips; dim [B,c] -> [B, c, 1]
+    attack_matrix_batch = unflat(attack.squeeze(2), C, H, W)
+    adversarial_batch = batch + attack_matrix_batch 
+    return adversarial_batch
